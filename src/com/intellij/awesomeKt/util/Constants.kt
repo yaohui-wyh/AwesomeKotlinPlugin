19c114af25ea83cf08c84308ea6f2e55b8fae6fc
Wang Yaohui <wangyaohuicn@gmail.com>
2018-11-22 14:39:09 +0800

    v1.0.0 Release

--- src/com/intellij/awesomeKt/util/Constants.kt
+++ src/com/intellij/awesomeKt/util/Constants.kt
@@ -9,8 +9,8 @@ object Constants {
     const val componentName = "AwesomeKotlin Application Component"
 
     // FIXME
-    const val pluginRateUrl = "https://github.com/alex-yh99/AwesomeKotlinPlugin"
-    const val issueUrl = "https://github.com/alex-yh99/AwesomeKotlinPlugin/issues"
+    const val pluginRateUrl = "https://github.com/roger-yh99/AwesomeKotlinPlugin"
+    const val issueUrl = "https://github.com/roger-yh99/AwesomeKotlinPlugin/issues"
     const val awesomeKotlinUrl = "https://github.com/KotlinBy/awesome-kotlin"
 
     const val propRefreshBtnBusy = "Property.RefreshButtonBusy"
