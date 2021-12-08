9694c07397c1c953788488b66935fe5d30634e6e
Wang Yaohui <wangyaohuicn@gmail.com>
2021-12-08 12:42:50 +0800

    feat: add publishPlugin gradle task

--- build.gradle.kts
+++ build.gradle.kts
@@ -83,6 +83,13 @@ tasks {
             }.toHTML()
         })
     }
+
+    publishPlugin {
+        dependsOn("patchChangelog")
+        token.set(System.getenv("PUBLISH_TOKEN"))
+        channels.set(listOf(properties("pluginVersion").split('-').getOrElse(1) { "default" }.split('.').first()))
+    }
+
     runIde {
         systemProperty("AwesomeKotlin.is.internal", "true")
     }
